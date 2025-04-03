c52d1f72cd59d0028bf89313d5ce5131177b776f
or_hyn <dhflxhdxhd@gmail.com>
2025-04-03 21:03:47 +0900

    setting: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-500">
+      <h1 className="text-white text-4xl font-bold p-4 rounded-lg shadow-lg bg-black/30">
+        Tailwind CSS 적용 완료! 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
